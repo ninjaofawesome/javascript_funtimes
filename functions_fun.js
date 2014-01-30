f2ba547e12fd4756b97c720ca3f4c7c708af7d7c
hannah nordgren <hannah.nordgren@flatironschool.com>
2014-01-30 13:16:33 -0500

    trying out a few things

---
+++ functions_fun.js
@@ -0,0 +1,15 @@
+
+// This prints out five things in a row
+var sayHooray = function(saying) {
+	var number = 5;
+	for(i = 0; i < number; i++) {
+	console.log(saying + " You did it!");
+	}
+}
+sayHooray("Hooray!");
+
+
+
+
+
+
